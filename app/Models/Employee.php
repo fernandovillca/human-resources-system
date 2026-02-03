864aa456e81688ca3f21c1f16782973368843a6f
fernando <vluisfernando62@gmail.com>
2026-02-03 12:06:22 -0700

    feat: definir relaciones Eloquent en modelo Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -13,4 +13,52 @@ class Employee extends Model
         'phone',
         'address',
     ];
+
+    public function designation()
+    {
+        return $this->belongsTo(Designation::class);
+    }
+
+    public function department()
+    {
+        return $this->designation->department;
+    }
+
+    public function scopeInCompany($query)
+    {
+        return $query->whereHas('designation', function ($q) {
+            $q->inCompany();
+        });
+    }
+
+    public function scopeSearchByName($query, $name)
+    {
+        return $query->where('name', 'LIKE', "%{$name}%");
+    }
+
+    public function salaries()
+    {
+        return $this->hasMany(Salary::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function cotracts()
+    {
+        return $this->hasMany(Contract::class);
+    }
+
+    public function getActiveContract($startDate = null, $endDate = null)
+    {
+        $startDate = $startDate ?? now();
+        $endDate = $endDate ?? now();
+
+        return $this->cotracts()
+            ->where('start_date', '<=', $startDate)
+            ->where('end_date', '>=', $endDate)
+            ->first();
+    }
 }
