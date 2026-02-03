eaca25ffb0ca29ece63e99fee0a27916fbc23bc4
fernando <vluisfernando62@gmail.com>
2026-02-03 12:22:22 -0700

    feat: definir relaciones Eloquent en modelo Payroll

--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Payroll extends Model
@@ -11,4 +12,34 @@ class Payroll extends Model
         'year',
         'month',
     ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function salaries()
+    {
+        return $this->hasMany(Salary::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function scopeInCompany($query)
+    {
+        return $query->where('company_id', $this->company_id);
+    }
+
+    public function getMonthYearAttribute()
+    {
+        return date('F Y', strtotime("{$this->year}-{$this->month}-01"));
+    }
+
+    public function getMonthStringAttribute()
+    {
+        return Carbon::parse($this->month_year)->format('F Y');
+    }
 }
