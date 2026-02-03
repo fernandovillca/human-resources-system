8a1d79ca5324742914e8ac8feb6f85c66d5e792c
fernando <vluisfernando62@gmail.com>
2026-02-03 11:03:17 -0700

    feat: crear modelo y migración para `Designation`

---
+++ app/Models/Designation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Designation extends Model
+{
+    protected $fillable = [
+        'department_id',
+        'name',
+    ];
+}
