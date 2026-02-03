fff3e89ce7065d39cef85e3071858df17bba0620
fernando <vluisfernando62@gmail.com>
2026-02-03 12:15:07 -0700

    feat: definir relaciones Eloquent en modelo Contract

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Contract extends Model
@@ -14,4 +15,40 @@ class Contract extends Model
         'rate_type',
         'rate',
     ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function designation()
+    {
+        return $this->belongsTo(Designation::class);
+    }
+
+    public function scopeInCompany($query)
+    {
+        return $query->whereHas('designation', function ($q) {
+            $q->inCompany();
+        });
+    }
+
+    public function getDurationAttribute()
+    {
+        return Carbon::parse($this->start_date)->diffForHumans($this->end_date);
+    }
+
+    public function scopeSearchByEmployee($query, $name)
+    {
+        return $query->whereHas('employee', function ($q) use ($name) {
+            $q->where('name', 'LIKE', "%{$name}%");
+        });
+    }
+
+    public function getTotalEarnings($monthYear)
+    {
+        return $this->rite_type === 'mensual'
+            ? $this->rate
+            : $this->rate * Carbon::parse($monthYear)->daysInMonth;
+    }
 }
